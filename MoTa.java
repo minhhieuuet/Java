75e279b0c307a7ba41853c07526935d609a6b63c
noproname1998 <noproname1998@gmail.com>
2017-09-15 01:13:44 +0700

    Sua lan 4

--- MoTa.java
+++ MoTa.java
@@ -9,158 +9,198 @@ package tuan2;
  *
  * @author MINH HIEU
  */
+//Khởi tạo class ConMeo
  class ConMeo {
+     //Khởi tạo đối tượng cân nặng
     private float CanNang;
+    //Khởi tạo đối tượng tên và loại mèo
     private String Ten,Loai;
+    //Hàm kêu
     void keu(){
         System.out.println("Meo meo");
     };
+    //Hàm set giá trị cho Cân nặng
      void setCanNang(float weight)
             {
                 this.CanNang=weight;
                 
             }
+     //Hàm set giá trị cho Tên
     void setTen(String name)
             {
                 this.Ten=name;
                 
             }
+    //Hàm set giá trị cho loại
     void setLoai(String type)
             {
                 this.Loai=type;
                 
             }
+    //Hàm lấy giá trị cân nặng
     float getCanNang()
     {
         return this.CanNang;
     }
+    //Hàm lấy tên
     String getTen()
     {
         return this.Ten;
     }
+    //Hàm lấy thể loại
     String getLoai()
     {
         return this.Loai;
     }
 }
-
+//Khởi tạo lớp GiaoVien
 class GiaoVien
 {
+    //Khởi tạo đối tượng tên và môn học
     private String Ten,MonHoc;
+    //Khởi tạo đối tượng tuổi
     private int Tuoi;
+    //Khởi tạo hàm giangbai
     void giangbai()
     {
         System.out.println("Mot Hai Ba");
     }
+    //Hàm set giá trị cho tên
     void setTen(String name)
     {
         this.Ten=name;
     }
+    //Hàm set giá trị cho môn học
     void setMonHoc(String subject)
     {
         this.MonHoc=subject;
     }
+    //Hàm set giá trị cho tuổi
     void setTuoi(int age)
     {
         this.Tuoi=age;
     }
+    //Hàm lấy giá trị của Ten
     String getTen()
     {
         return this.Ten;
     }
+    //hàm lấy giá trị của MonHoc
     String getMonHoc()
     {
         return this.MonHoc;
     }
+    //Hàm lấy giá trị của Tuoi
     int getTuoi()
     {
         return this.Tuoi;
     }
 }
-
+//Lớp máy bay
         class MayBay{
+            //Khởi tạo đối tượng Ten và Loai
             private String Ten,Loai;
+            //Khởi tạo đối tương SoKhach
             private int SoKhach;
+            //Khởi tạo hàm Bay
             void Bay
             {
                 System.out.println("u u u");
             }
+            //Hàm set giá trị cho Ten
             void setTen(String name)
             {
                 this.Ten=name;
             }
+            //Hàm set giá trị cho Loai
             void setLoai(String type)
             {
                 this.Loai=type;
             }
+            //Hàm set giá trị cho SoKhach
             void setSoKhach (int sl)
             {
                 this.SoKhach=sl;
             }
+            //Hàm lấy giá trị của tên
             String getTen()
             {
                 return this.Ten;
             }
-            String Loai()
+            //Hàm lấy giá trị của Loai
+            String getLoai()
             {
                 return this.Loai;
             }
+            //Hàm lấy giá trị của SoKhach
             int getSoKhach()
             {
                 return this.SoKhach;
             }
                     
 }
-          
+         //Khởi tạo lớp Cay
         class Cay{
+            //Khởi tạo đối tượng ChieuCao
             private float ChieuCao;
+            //Khởi tạo đối tượng Loai
             private String Loai;
-            
+            //Hàm set giá trị cho ChieuCao
             void setChieuCao(float height)
             {
                 this.ChieuCao=height;
             }
+            //Hàm set giá trị cho Loai
             void setLoai(String type)
             {
                 this.Loai=type;
             }
+            //Hàm lấy giá trị của chiều cao
            float getChieuCao ()
             {
                 return this.ChieuCao;
-            }               
+            }             
+           //Hàm get giá trị của Loai
            String getLoai()
            {
                 return this.Loai;
            }
  }
+        //Khởi tạo lớp SinhVien
          class SinhVien{
-         
+         //Khởi tạo đối tượng Ten, MSSV,Khoa kiểu String
              private String Ten,MSSV,Khoa;
+             //Khởi tạo hàm study
              void study()
              {
              
              }
+             //Hàm set giá trị cho ten
              void setTen(String name)
              {
                  this.Ten=name;
              }
+             //Hàm set giá trị cho MSSV
              void setMSSV(String MS)
              {
                  this.MSSV=MS;
              }
+             //Hàm set giá trị cho Khoa
              void setKhoa(String n)
              {
                  this.Khoa=n;
              }
-             
+             //Hàm lấy giá trị của Ten
              String getTen()
              {
                  return this.Ten;
              }
+             //Hàm lấy giá trị của MSSV
              String getMSSV()
              {
                  return this.MSSV;
              }
+             //Hàm lấy giá trị của Khoa
              String getKhoa()
              {
                  return this.Khoa;
@@ -168,101 +208,130 @@ class GiaoVien
          
          
          }
+         //Khởi tạo lớp CauThu
         class CauThu{
+            //Khởi tạo đối tượng SoAo kiểu int
             int SoAo;
+            //Khởi tạo đối tượng Ten,Doi kiểu String
             String Ten,Doi;
-            
+            //Hàm set giá trị cho Ten
             void setTen(String name)
             {
                 this.Ten=name;
             }
+            //Hàm set giá trị cho Doi
             void setDoi(String td)
             {
                 this.Doi=td;
             }
+            //Ham set gia tri cho SoAo
             void setSoAo(int number)
             {
                 this.SoAo=number;
             }
+            //Hàm lấy giá trị của Ten
             String getTen()
             {
                 return this.Ten;
             }
+            //Hàm lấy giá trị của đối tượng Doi
             String getDoi()
             {
                 return this.Doi;
             }
+            //Ham lấy giá trị của SoAo
             int getSoAo()
             {
                 return this.SoAo;
             }
         } 
-        
-        public class Sach{
+        //Khởi tạo lớp Sach
+        class Sach{
+            //Khởi tạo đổi tượng SoTrang,GiaTien
             int SoTrang,GiaTien;
+            //Khởi tạo đối tượng Loai kiểu String
             String Loai;
+            //Hàm set giá trị cho SoTrang
             void setSoTrang(int st)
             {
                 this.SoTrang=st;
             }
+            //hàm set giá trị cho GiaTien
             void setGiaTien(int gt)
             {
                 this.GiaTien=gt;
             }
+            //Hàm set giá trị cho Loai
             void setLoai(String l)
             {
                 this.Loai=l;
             }
+            //Hàm lấy giá trị Loai
             String getLoai()
             {
                 return this.Loai;
             }
+            //Hàm lấy giá trị của GiaTien
             int getGiaTien()
             {
                 return this.GiaTien;
             }
+            //Hàm lấy giá trị của SoTrang
             int getSoTrang()
             {
                 return this.SoTrang;
             }
         }
-        
+        //Khởi tạo lớp Car
          class Car{
+             //Khởi tạo đối tượng Ten,Loai,MauSac kiểu String
             String Ten,Loai,MauSac;
+            //Khởi tạo đổi tượng GiaTien kiểu int
             int GiaTien
+            //Khởi tạo hàm Chay
             void Chay{
                     System.out.println("brum brum");
             }
+            //Hàm set giá trị cho Ten
             void setTen(String name)
             {
                 this.Ten=name;
             }
+            //Hàm set giá trị cho Loai
             void setLoai(String loai)
             {
                 this.Loai=loai;
             }
+            //Hàm set giấ trị cho MauSac 
             void setMauSac(String colour)
             {
                 this.MauSac=colour;
             }
+            //Hàm set giá trị cho GiáTien
             void setGiaTien(int price)
             {
                 this.GiaTien=price;
             }
+            //Hàm lấy giá trị Ten
             String getTen()
             {
                 return this.Ten;
             }
+            //Hàm lấy giá trị Loai
             String getLoai()
             {
                 return this.Loai;
             }
+            //Hàm lấy giá trị MauSac
             String getMauSac()
             {
                 return this.MauSac;
             }
+            //Hàm lấy giá trị GiaTien
             int getGiaTien()
             {
                 return this.GiaTien;
             }
         }
+
+       
